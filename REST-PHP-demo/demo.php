168771e713396104daaabe64ceebc1606acd64ad
Mosuan <934817794@qq.com>
2019-09-06 15:41:48 +0800

    Update demo.php

--- REST-PHP-demo/demo.php
+++ REST-PHP-demo/demo.php
@@ -1,5 +1,5 @@
 <?php
-public function aesEncrypt($stringToEncrypt) {
+function aesEncrypt($stringToEncrypt) {
         $encryptKey  = env('AES_COMMON_KEY', '');
         $sql = "SELECT TO_BASE64(AES_ENCRYPT('{$stringToEncrypt}', '{$encryptKey}'))";
         $res = Yii::$app->db->createCommand($sql)->queryColumn();
